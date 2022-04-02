26399109b545a71bfe943221234ab06a7a4c8040
Fernando Santillan <ferdiego88@gmail.com>
2022-04-01 21:25:25 -0500

    Buscador Final

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -11,12 +11,15 @@ export class PorPaisComponent {
   termino = '';
   hayError = false;
   paises: Pais[] = [];
+  paisesSugeridos: Pais[] = [];
+  mostrarSugerencias = false;
   constructor(private paisService: PaisService) {
   }
 
 
   buscar(termino: string) {
     this.hayError = false;
+    this.mostrarSugerencias = false;
     this.termino = termino;
     this.paisService.buscarPais(this.termino)
       .subscribe({
@@ -29,6 +32,14 @@ export class PorPaisComponent {
 
   sugerencias(termino: string){
     this.hayError = false;
+    this.mostrarSugerencias = true;
+    this.termino = termino;
+    this.paisService.buscarPais(termino)
+    .subscribe({
+      next: (paises) => this.paisesSugeridos = paises.splice(0,4),
+      error:(error) => {console.error(error); this.hayError = true; this.paisesSugeridos = []},
+      complete:() => console.info('complete')
+    })
   }
 
 }
